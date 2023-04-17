cf01373cff1aee71f82b9cac07d77de2f0a5c820
SevenbitsVivek <vivek@sevenbits.in>
2023-04-17 16:25:11 +0530

    Made minor changes in WelcomePage.js file under src folder

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const WelcomePage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Welcome to Zk-Rollup POC</h1>
+      <h1>Welcome to ZK-Rollup POC</h1>
     </div>
   );
 };
