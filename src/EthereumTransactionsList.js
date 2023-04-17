ad7d13c8c0c786195b8ee923f35648fdc17ed0b7
SevenbitsVivek <vivek@sevenbits.in>
2023-04-17 19:26:56 +0530

    Resolved bugs in EthereumTransactionsList.js and PolygonTransactionsList.js file under src folder

--- src/EthereumTransactionsList.js
+++ src/EthereumTransactionsList.js
@@ -6,16 +6,20 @@ export default function StartPolygonTransactonIndexer() {
   const [transactionIndexer, setTransactionIndexer] = useState([]);
   const [decodedValue, setDecodedValue] = useState([]);
   useEffect(() => {
-    fetch(`https://api-goerli.etherscan.io/api?module=account&action=txlist&address=${'0xCF7810FF3339aBc68401233d37baef9674bBF84D'}&startblock=${8807099}&endblock=${99999999}&sort=asc&apikey=${'JB7KZVSGD7Z4AGJGEYITX4WY1W5V4I5D1K'}`)
+    fetch(`https://api-goerli.etherscan.io/api?module=account&action=txlist&address=${'0xCF7810FF3339aBc68401233d37baef9674bBF84D'}&startblock=${8807071 + 1}&endblock=${99999999}&sort=asc&apikey=${'JB7KZVSGD7Z4AGJGEYITX4WY1W5V4I5D1K'}`)
       .then((response) => response.json())
       .then((transactionIndexer) => {
         setTransactionIndexer(transactionIndexer.result);
         let resultArray = []
         for (var i = 0; i < transactionIndexer.result.length; i++) {
-          const data = transactionIndexer.result[i].input
-          const decoder = new InputDataDecoder(polygonAbi.abi);
-          const result = decoder.decodeData(data);
-          resultArray.push(result.inputs[1])
+          if (transactionIndexer.result[i].txreceipt_status === "1") {
+            const data = transactionIndexer.result[i].input
+            const decoder = new InputDataDecoder(polygonAbi.abi);
+            const result = decoder.decodeData(data);
+            if (result.method === "setBase64") {
+              resultArray.push(result.inputs[1])
+            }
+          }
         }
         setDecodedValue(resultArray)
       });
--- src/PolygonTransactionsList.js
+++ src/PolygonTransactionsList.js
@@ -6,16 +6,20 @@ export default function StartPolygonTransactonIndexer() {
   const [transactionIndexer, setTransactionIndexer] = useState([]);
   const [decodedValue, setDecodedValue] = useState([]);
   useEffect(() => {
-    fetch(`https://api-testnet.polygonscan.com/api?module=account&action=txlist&address=${'0x87c60581A565FC1D25Ffd126FFd14965d36161a2'}&startblock=${34205366}&endblock=${99999999}&sort=asc&apikey=${'61NXGEUMZJGEXU5ZTZQN8ZGHRBC8PAVSFN'}`)
+    fetch(`https://api-testnet.polygonscan.com/api?module=account&action=txlist&address=${'0x87c60581A565FC1D25Ffd126FFd14965d36161a2'}&startblock=${34205248 + 1}&endblock=${99999999}&sort=asc&apikey=${'61NXGEUMZJGEXU5ZTZQN8ZGHRBC8PAVSFN'}`)
       .then((response) => response.json())
       .then((transactionIndexer) => {
         setTransactionIndexer(transactionIndexer.result);
         let resultArray = []
         for (var i = 0; i < transactionIndexer.result.length; i++) {
-          const data = transactionIndexer.result[i].input
-          const decoder = new InputDataDecoder(polygonAbi.abi);
-          const result = decoder.decodeData(data);
-          resultArray.push(result.inputs[1])
+          if (transactionIndexer.result[i].txreceipt_status === "1") {
+            const data = transactionIndexer.result[i].input
+            const decoder = new InputDataDecoder(polygonAbi.abi);
+            const result = decoder.decodeData(data);
+            if (result.method === "setBase64") {
+              resultArray.push(result.inputs[1])
+            }
+          }
         }
         setDecodedValue(resultArray)
       });
